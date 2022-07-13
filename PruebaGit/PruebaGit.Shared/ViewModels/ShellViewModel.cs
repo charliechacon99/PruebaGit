eeeb9d168e6c0316a8fb735be068b85f704b0a54
charliechacon99 <carlos99chacon@gmail.com>
2022-07-12 20:20:29 -0500

    Se egrega el proyecto

---
+++ PruebaGit/PruebaGit.Shared/ViewModels/ShellViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using PruebaGit.Core.Mvvm;
+using System;
+
+namespace PruebaGit.Shared.ViewModels
+{
+    public class ShellViewModel : ViewModelBase
+    {
+        public ShellViewModel()
+        {
+            Title = "Main Page";
+        }
+    }
+}
